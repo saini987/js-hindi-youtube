c5dcdc0fcd685d22589afbd1699b18b618231075
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-06 12:27:34 +0000

    hi

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const account_Id = 1564778
+let accountEmail = "rohit@gmail.com"
+var password = "252630"
+accountCity = "meerut"
+
+accountEmail = "mohit@gmail.com"
+password = "858650"
+accountCity = "saharanpur"
+
+console.table([account_Id,accountEmail,password,accountCity])
