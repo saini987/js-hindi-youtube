08081f7f0e59232dbf56aafb82a67321b8996db4
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-06 13:04:06 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,11 @@
+"use strict";
+let age = 22
+console.log(age)
+console.log(typeof age)
+
+let name ="rohit"
+let birth = 22
+let place = "roorkee"
+let house = "saharanpur"
+console.table([name,birth,place,house])
+console.table((typeof[name,birth,place,house]))
