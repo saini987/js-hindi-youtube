00019c725af06f59030845e36bf9a95b4b648a65
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-07 12:53:47 +0000

    dataTypeSummary

---
+++ 01_basics/05dataTypeSummary.js
@@ -0,0 +1,39 @@
+//primative data type
+// this is string data type
+const name = "ROHIT KUMAR SAINI"
+
+//this is number data type
+const score = 45
+
+//this is boolearn data type
+const InLogin = false
+
+//this is null data type
+const UserNumber = null
+
+//this is undefined data type
+const UserEmail = undefined
+
+// this is symbol data type
+const id = Symbol("1245")
+
+//this is bigint data type
+const number = 1223456566n
+
+console.log(id)
+
+// non primative data type
+//array
+const brothers = ["MOHIT","RAHUL","ANSHUL","LAKSHAY"];
+
+//objact
+let myObjact = {
+    name: "ROHIT",
+    age: 22.
+}
+
+// function
+let hello = function(){
+    console.log("HI ROHIT");
+}
+console.log(typeof brothers)
