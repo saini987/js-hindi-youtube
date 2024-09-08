745c48f1f08935481247391dc01b4aaf1ddc8fc0
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-08 10:46:16 +0000

    split

--- 01_basics/06_staring.js
+++ 01_basics/06_staring.js
@@ -10,3 +10,15 @@ console.log(gameName.length)
 console.log(gameName.bold())
 console.log(gameName.charAt(6))
 console.log(gameName.indexOf('k'))
+
+const newstring = gameName.substring(0,4)
+console.log(newstring)
+
+const new2 = gameName.slice(-3,1)
+console.log(new2)
+
+console.log(gameName.replace('r',('m')))
+
+// string -> array
+
+console.log(gameName.split(' '))
