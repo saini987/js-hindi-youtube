d8998ee99dc46f9d5761cc684b5a59e95d2c9f63
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-08 07:11:35 +0000

    new string

--- 01_basics/06_staring.js
+++ 01_basics/06_staring.js
@@ -1,4 +1,12 @@
 const name = "Rohit"
 const sarname = "Saini"
 
-console.log(`my name is ${name}  and my sarname is ${sarname}` )
+//console.log(`my name is ${name}  and my sarname is ${sarname}` )
+
+const gameName = new String('rohit kumar saini')
+console.log(gameName[6])
+console.log(gameName.toUpperCase())
+console.log(gameName.length) 
+console.log(gameName.bold())
+console.log(gameName.charAt(6))
+console.log(gameName.indexOf('k'))
