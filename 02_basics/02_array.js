1cd41d3d56a2d0a94d9b840e3db12b7c5a147aea
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-13 14:31:48 +0000

    Arrayof

---
+++ 02_basics/02_array.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros =["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// let addArray = [marvel_heros+dc_heros]
+// console.log(addArray)
+
+
+// console.log(addArray[2])
+
+// let cont = marvel_heros.concat(dc_heros)
+// console.log(cont)
+
+// let allHeros = [...marvel_heros,...dc_heros]
+// console.log(allHeros)
+
+// const anotherArray = [1,2,3,4,[4,5,6],2,[78,4,5,[6,9,3]]]
+// const realAnthoerArray = anotherArray.flat(Infinity)
+// console.log(realAnthoerArray)
+
+console.log(Array.isArray("ROHIT"))
+console.log(Array.from("ROHIT"))
+console.log(Array.from("23567"))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
