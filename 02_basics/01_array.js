d42e944dc7c414c701a0fc7a2caea1d5ac58000d
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-13 13:08:59 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+const myArr = [12,13,14,15,16]
+
+const herros = ['saktiman','rohit']
+
+const myArr2 = new Array(12,45,78,89,56,23)
+
+// console.log(myArr[1])
+// console.log(herros)
+// console.log(myArr2)
+
+// herros.push('mohit','anshul')
+// console.log(herros)
+
+// herros.pop()
+// console.log(herros)
+
+// herros.unshift('rahul')
+// console.log(herros)
+
+// console.log(myArr.includes(15))
+// console.log(myArr.indexOf(15))
+
+// const newArr =  myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+// slice, splice
+
+console.log("A" ,myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B" ,myArr)
+const myn2 = myArr.splice(1,3)
+
+console.log("c",myArr)
+console.log(myn2);
+
+console.log(myArr)
