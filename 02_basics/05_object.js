f977eb3f7675d81677c2886ce1022fbf2d220d72
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-14 13:42:04 +0000

    destructor

---
+++ 02_basics/05_object.js
@@ -0,0 +1,8 @@
+const  course = {
+    courseName : "js in hindi",
+    pricecs : "999",
+    courseInstructor:"hitesh Sir"
+}
+const {courseInstructor : ins} = course
+// console.log(courseInstructor)
+console.log(ins)
