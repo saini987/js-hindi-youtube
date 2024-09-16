cc6a5f5f93a639b380e8e4109a100cbfd204bc56
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-16 17:43:33 +0000

    question

--- 02_basics/test.js
+++ 02_basics/test.js
@@ -1,21 +1,42 @@
-// Array Reversal
+// // Array Reversal
 
-const name = [1,2,3,4,5,6,7,8,9]
+// const name = [1,2,3,4,5,6,7,8,9]
 
-console.log(name.reverse())
+// console.log(name.reverse())
 
-// Sum of Elements
+// // Sum of Elements
 
-let valve = [1, 2, 3, 4, 5,6];
-function sumValve(arr) {
-    return arr.reduce((acc, val) => acc + val, 0);
+// let valve = [1, 2, 3, 4, 5,6];
+// function sumValve(arr) {
+//     return arr.reduce((acc, val) => acc + val, 0);
+// }
+// console.log(sumValve(valve));
+
+// //Finding the Maximum
+
+// let array = [12,89,8,481,9,567,785,15789,585,50,8956,2389,5978,687912,5792,14697,];
+// function maxArray(arr){
+//     return Math.max(...arr);
+// }
+// console.log(maxArray(array));
+
+// // Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, 
+// discount it by 7%. Update the object with :
+
+const product = {
+    name : "Laptop",
+    prices : 46999,
+    discount : 0,
 }
-console.log(sumValve(valve));
+console.log(Object.entries(product))
 
-//Finding the Maximum
-
-let array = [12,89,8,481,9,567,785,15789,585,50,8956,2389,5978,687912,5792,14697,];
-function maxArray(arr){
-    return Math.max(...arr);
+if(product.prices >= 46000){
+    discount = ((product.prices/100)*10);
+    product.prices = (product.prices - discount);
 }
-console.log(maxArray(array));
+
+else{
+    discount = ((product.prices/100)*7);
+    product.prices = (product.prices - discount);
+}
+console.log(Object.entries(product))
