0ac98f6f9bb2c862baee44ca3915fce3df37e49c
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-19 13:30:02 +0000

    switch

---
+++ 04_control_Flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const July = 7
+
+switch(July){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("april");
+        break;        
+    case 7:
+        console.log("July");
+        break;   
+    case 8:
+        console.log("Aug");
+        break;  
+
+        default:
+    console.log("condetion not match")
+    break     
+}
