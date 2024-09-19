a90285796b579da1a91a29d2de62f869f939c824
ROHIT SAINI <rksaini9320@gmail.com>
2024-09-19 13:07:07 +0000

    IF ELSE

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,21 @@
+let temp = 42
+
+if(temp == 41){
+    console.log("Temp Less then is 50")
+}
+else{
+    console.log("Temp graten then is 50 : ya Less then 50")
+}
+console.log("IF ELSE condison is excuted") 
+
+const userLoggIn = true
+const debitCard = true
+const LoggINGoogle = false
+const LoggINGmail = true
+
+if(userLoggIn && debitCard && 2==33){
+    console.log("Allow to buy courise")
+}
+if(LoggINGoogle || LoggINGmail){
+    console.log("Welcome")
+}
